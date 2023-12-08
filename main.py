62d857290f6aaf6306c8af35aa9d6f209569bd7c
Tsz W. TAM <tszw.tam+github@gmail.com>
2023-12-08 01:03:10 -0800

    added Google Map url

--- main.py
+++ main.py
@@ -21,41 +21,53 @@ def get_distance_matrix(cities_location: dict[list]):
     for i in range(len(cities)):
         for j in range(len(cities)):
             if i == j:
-                distance_matrix[i][j] = 1
+                distance_matrix[i][j] = 0
             else:
                 city1_location = cities_location[cities[i]]
                 city2_location = cities_location[cities[j]]
 
-                # Set distance to infinity if it's Vancouver to New York or vice versa
-                if (cities[i] == "Vancouver" and cities[j] == "New York") or (
-                    cities[i] == "New York" and cities[j] == "Vancouver"
-                ):
-                    distance_matrix[i][j] = float("inf")
-                else:
-                    distance_matrix[i][j] = int(
-                        geodesic(city1_location, city2_location).km
-                    )
+                distance_matrix[i][j] = int(geodesic(city1_location, city2_location).km)
 
     return distance_matrix
 
 
+def get_map_url(cities):
+    print(cities)
+
+    url = "https://www.google.com/maps/dir/"
+    for city in cities:
+        city_name = city[0].replace(" ", "+")
+        state = city[1].replace(" ", "+")
+        country = city[2].replace(" ", "+")
+
+        params = f"{city_name},{state},{country}/"
+        url += params
+    return url
+
+
 if __name__ == "__main__":
+    cities_info = []
     city_location_mapping = {}
 
     with open("city.txt", "r") as file:
         for line in file:
             city, state, country = line.strip().split(",")
+            cities_info.append((city, state, country))
 
-            location = get_location(city=city, state=state, country=country)
-            city_location_mapping[city] = location
+    for city in cities_info:
+        location = get_location(*city)
+        city_location_mapping[city] = location
 
     distance_matrix = get_distance_matrix(city_location_mapping)
 
-    path = dijkstra(distance_matrix)
+    order = dijkstra(distance_matrix)
+    ordered_cities_info = [cities_info[i] for i in order]
 
-    cities = list(city_location_mapping.keys())
-
-    route = [cities[city] for city in path]
+    route = [city[0] for city in ordered_cities_info]
+    formatted_route = " -> ".join(route)
 
     print("Travel Route: ")
-    print(" -> ".join(route))
+    print(formatted_route)
+
+    map_url = get_map_url(ordered_cities_info)
+    print(map_url)
