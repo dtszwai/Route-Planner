70059eae07e0e300e82221065494c52ee04df9e8
Tsz W. TAM <tszw.tam+github@gmail.com>
2023-12-08 09:37:46 -0800

    styled printing messages

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from geopy.distance import geodesic
 from dijkstra import dijkstra
+from rich.console import Console
+from rich.table import Table
+
 
 API_KEY = ""
 
@@ -43,11 +46,11 @@ def get_map_url(cities):
     return url
 
 
-if __name__ == "__main__":
+def get_travel_route(path="city.txt"):
     cities_info = []
     city_location_mapping = {}
 
-    with open("city.txt", "r") as file:
+    with open(path, "r") as file:
         for line in file:
             city, state, country = line.strip().split(",")
             cities_info.append((city, state, country))
@@ -61,15 +64,31 @@ if __name__ == "__main__":
     path_order = dijkstra(distance_matrix)
     ordered_cities_info = [cities_info[i] for i in path_order]
 
-    route = [city[0] for city in ordered_cities_info]
-    formatted_route = " -> ".join(route)
     total_distance = sum(
         distance_matrix[a][b] for a, b in zip(path_order, path_order[1:])
     )
 
-    print("Travel Route: ")
-    print(formatted_route)
-    print(f"Total Distance: {total_distance}km")
+    return (ordered_cities_info, total_distance)
+
+
+def print_result(cities, distance):
+    console = Console()
+
+    route = [city[0] for city in cities]
+    formatted_route = " -> ".join(route)
 
     map_url = get_map_url(ordered_cities_info)
-    print(map_url)
+
+    table = Table(expand=True)
+    table.add_column("Travel Route", justify="center", style="bold cyan", no_wrap=True)
+
+    table.add_row(f"[green]Total Distance: {str(distance)}km[/green]")
+    table.add_row(f"[blue]{formatted_route}[/blue]")
+    table.add_row(f"[yellow][link={map_url}]Google Maps Route[/link][/yellow]")
+
+    console.print(table)
+
+
+if __name__ == "__main__":
+    ordered_cities_info, total_distance = get_travel_route(path="city.txt")
+    print_result(ordered_cities_info, total_distance)
